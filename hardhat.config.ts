8a1d4a19d43a0e36137f8084eee1b17741d3636e
J <phunhtk24414h@st.uel.edu.vn>
2026-05-14 13:43:34 +0700

    hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
-   import "@nomicfoundation/hardhat-toolbox-viem";
+   import "@nomicfoundation/hardhat-toolbox";
    import * as dotenv from "dotenv";
 
    dotenv.config();
 
    const config: HardhatUserConfig = {
-     solidity: "0.8.24", // Phiên bản Solidity bạn dùng
+     solidity: "0.8.28",
      networks: {
-       sepolia: {
-         url: process.env.SEPOLIA_RPC_URL || "",
-         accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      sepolia: {
+        type: "http",
+        url: process.env.SEPOLIA_RPC_URL || "",
+        accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
        },
      },
    };
