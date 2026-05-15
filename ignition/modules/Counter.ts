8137d3e60518b7fbb8a26b035c8d1c9d560ae6af
J <phunhtk24414h@st.uel.edu.vn>
2026-05-15 15:29:51 +0700

    Counter.ts

--- ignition/modules/Counter.ts
+++ ignition/modules/Counter.ts
@@ -1,9 +1,7 @@
 import { buildModule } from "@nomicfoundation/ignition-core";
 
 export default buildModule("CounterModule", (m) => {
-  const counter = m.contract("Counter");
-
-  m.call(counter, "incBy", [5n]);
-
+  const admin = m.getAccount(0);
+  const counter = m.contract("Counter", [admin]); 
   return { counter };
 });
